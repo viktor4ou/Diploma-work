205ed03e805b91c5789e4b9da06189ffa989684e
Viktor Georgiev <vikisurf95@gmail.com>
2022-12-12 01:51:21 +0200

    added js

---
+++ index.js
@@ -0,0 +1,14 @@
+let myCart = document.getElementById("myCart") //span = "0";
+let myCartButton = document.getElementById("myCartButton") //
+let counter = 0;
+
+myCartButton.addEventListener("click",seksbog34slqto) // ako se natisne butona se pravi tova
+
+function seksbog34slqto (){
+
+myCart.textContent = `${counter += 1}`
+    if(myCart.textContent >= 99)
+    {
+        myCart.textContent = "99+";
+    }
+}
