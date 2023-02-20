e12fcdb9960b5c88acfdc6dd42aa982cb749dd17
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2023-02-20 22:02:14 +0200

    added working cart

---
+++ DataBase/data.js
@@ -0,0 +1,21 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Intel i3-12100f",
+    price: 229,
+    desc: `<i class="bi bi-check-lg"></i> 4 ядра / 8 нишки
+    <br><i class="bi bi-check-lg"></i> 3.30 - 4.30 GHz
+    <br><i class="bi bi-check-lg"></i> 15MB кеш `,
+    img: "../img/CPU/intel_core_i312100f.jpg",
+  },
+  {
+    id: "firstName",
+    name: "Intel i3-12100f",
+    price: 229,
+    desc: `<i class="bi bi-check-lg"></i> 4 ядра / 8 нишки
+    <br><i class="bi bi-check-lg"></i> 3.30 - 4.30 GHz
+    <br><i class="bi bi-check-lg"></i> 15MB кеш `,
+    img: "../img/CPU/intel_core_i312100f.jpg",
+  },
+  
+];
