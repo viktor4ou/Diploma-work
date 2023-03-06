58886c621f738d4769ce7ddd8daa84aada9e3352
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2023-03-06 13:52:22 +0200

    added searchbar

---
+++ DataBase/searchData.js
@@ -0,0 +1,101 @@
+export let  shopItemsData = [
+    {
+      id: "intel_i3_12100f",
+      name: "Intel Core i3-12100F",
+      price: 229,
+      desc: `<i class="bi bi-check-lg"></i> 4 ядра / 8 нишки
+      <br><i class="bi bi-check-lg"></i> 3.30 - 4.30 GHz
+      <br><i class="bi bi-check-lg"></i> 15MB кеш `,
+      img: "../../img/CPU/intel_core_i312100f.jpg",
+      url: "../CPU/i3-12100f.html"
+    },
+    {
+      id: "ryzen_5_3600",
+      name: "AMD Ryzen 5 3600",
+      price: 229,
+      desc: `
+      <i class="bi bi-check-lg"></i> 4 ядра / 12 нишки
+      <br><i class="bi bi-check-lg"></i> 3.60 - 4.20 GHz
+      <br><i class="bi bi-check-lg"></i> 32MB L3 кеш 
+      `,
+      img: "../../img/CPU/amd_ryzen_5_3600.jpg",  
+    },
+    {
+      id: "intel_i5_10400f",
+      name: "Intel i5-10400F",
+      price: 239,
+      desc: `
+      <i class="bi bi-check-lg"></i> 6 ядра / 12 нишки
+      <br><i class="bi bi-check-lg"></i> 2.90 - 4.30 GHz
+      <br><i class="bi bi-check-lg"></i> 12MB кеш 
+      `,
+      img: "../../img/CPU/intel_core_i510400f.jpg",
+    },
+    {
+      id: "ryzen_5_5600",
+      name: "AMD Ryzen 5 5600",
+      price: 239,
+      desc: `
+      <i class="bi bi-check-lg"></i> 4 ядра / 12 нишки
+      <br><i class="bi bi-check-lg"></i> 3.50 - 4.40 GHz
+      <br><i class="bi bi-check-lg"></i> 32MB L3 кеш 
+      `,
+      img: "../../img/CPU/amd_ryzen_5_5600.jpg",
+    },
+    {
+      id: "ryzen_7_5700x",
+      name: "AMD Ryzen 7 5700",
+      price: 479,
+      desc: `
+      <i class="bi bi-check-lg"></i> 8 ядра / 16 нишки
+      <br><i class="bi bi-check-lg"></i> 3.40 - 4.60 GHz
+      <br><i class="bi bi-check-lg"></i> 32MB L3 кеш 
+      `,
+      img: "../../img/CPU/amd_ryzen_7_5700x.jpg",
+    },
+    {
+      id: "intel_i5_11600kf",
+      name: "Intel Core i5-11600KF",
+      price: 229,
+      desc: `
+      <i class="bi bi-check-lg"></i> 6 ядра / 12 нишки
+      <br><i class="bi bi-check-lg"></i> 3.90 - 4.90 GHz
+      <br><i class="bi bi-check-lg"></i> 12МБ Smart кеш 
+      `,
+      img: "../../img/CPU/Intel Core i5-11600KF.jpg",
+    },
+    {
+      id: "ryzen_5_7600x",
+      name: "AMD Ryzen 5 7600X",
+      price: 599,
+      desc: `
+      <i class="bi bi-check-lg"></i> 6 ядра / 12 нишки
+      <br><i class="bi bi-check-lg"></i> 4.70 - 5.30 GHz
+      <br><i class="bi bi-check-lg"></i> 32MB L3 кеш 
+      `,
+      img: "../../img/CPU/amd_ryzen_5_7600x.png",
+    },
+    {
+      id: "intel_i5_12600KF",
+      name: "Intel Core i5-12600KF",
+      price: 629,
+      desc: `
+      <i class="bi bi-check-lg"></i> 10 ядра / 16 нишки
+      <br><i class="bi bi-check-lg"></i> 3.70 - 4.90 GHz
+      <br><i class="bi bi-check-lg"></i> 20MB Intel кеш 
+      `,
+      img: "../../img/CPU/intel_core_i5_12600kf.jpg",
+    },
+    {
+      id: "ryzen_9_5950x",
+      name: "AMD Ryzen 5 3600",
+      price: 229,
+      desc: `
+      <i class="bi bi-check-lg"></i> 16 ядра / 32 нишки
+      <br><i class="bi bi-check-lg"></i> 3.40 - 4.90 GHz
+      <br><i class="bi bi-check-lg"></i> 64MB L3 кеш 
+      `,
+      img: "../../img/CPU/and_ryzen_9_5950x.jpg",
+    },
+  ];
+
---
+++ search.js
@@ -0,0 +1,48 @@
+import {shopItemsData} from './DataBase/searchData.js';	
+console.log(shopItemsData)
+let search = document.getElementById('search');
+let search_icon = document.getElementById('search_icon');
+
+search_icon.addEventListener('click', () => {
+    search.classList.toggle('search_input');
+})
+
+let search_bx2 = document.getElementsByClassName('search_bx2')[0];
+
+
+window.addEventListener('load', () => {
+    shopItemsData.forEach(element => {
+        const { img, name, url } = element;
+
+        let card = document.createElement('a');
+        card.href = url;
+        card.innerHTML = ` <img src="${img}" alt="">
+        <div class="content2">
+            <h6>${name}</h6>
+        </div>`;
+        search_bx2.appendChild(card);
+    });
+});
+
+
+search.addEventListener('keyup', () => {
+    let filter = search.value.toUpperCase();
+    let a = search_bx2.getElementsByTagName('a');
+    for (let i = 0; i < a.length; i++) {
+        let b = a[i].getElementsByClassName('content2')[0];
+        let c = b.getElementsByTagName('h6')[0];
+
+        let TextValue = c.textContent || c.innerText;
+        if (TextValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = '';
+            search_bx2.style.visibility = "visible";
+            search_bx2.style.opacity = 1;
+        } else {
+            a[i].style.display = 'none';
+        }
+        if (search.value == 0) {
+            search_bx2.style.visibility = "hidden";
+            search_bx2.style.opacity = 0;
+        }
+    }
+})
